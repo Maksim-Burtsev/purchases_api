4188964a84f5dcadf393dbb70aabf8d0afe75ed3
MaximBurtsev <zadrot-lol@list.ru>
2022-07-17 22:26:06 +0300

    add tests for /delete_note

--- tests/test_notes_api.py
+++ tests/test_notes_api.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi.testclient import TestClient
 
 from api.main import app
@@ -11,12 +12,12 @@ client = TestClient(app)
 def test_add_one_note():
     response = client.post(
         "/add_notes",
-        json=[{"title": "test", "tag": "test_tag", "date": "2022-07-17"}]
+        json=[{"title": "test", "tag": "tag1", "date": "2022-07-17"}]
     )
 
     assert response.status_code == 201
     assert response.json() == [
-        {'date': '2022-07-17', 'tag': 'test_tag', 'title': 'test'}]
+        {'date': '2022-07-17', 'tag': 'tag1', 'title': 'test'}]
 
 
 def test_add_list_notes():
@@ -34,6 +35,25 @@ def test_add_list_notes():
         {'date': '2022-01-17', 'tag': 'tag2', 'title': 'test2'},
         {'date': '2022-11-17', 'tag': 'tag1', 'title': 'test3'}]
 
+def test_wrong_delete():
+
+    response = client.delete('/delete_note')
+    assert response.status_code == 400
+
+def test_delete_notes_by_title():
+    response = client.delete('/delete_note?title=string')
+
+    assert response.status_code == 200
+
+def test_delete_notes_by_tag():
+    db = SessionLocal()
+    assert len(db.query(Note).filter(Note.tag=='tag1').all()) == 2
+
+    response = client.delete('/delete_note?tag=tag1')
+    assert response.status_code == 200
+
+    assert len(db.query(Note).filter(Note.tag=='tag1').all()) == 0
+     
 
 def test_tear_down():
     db = SessionLocal()
